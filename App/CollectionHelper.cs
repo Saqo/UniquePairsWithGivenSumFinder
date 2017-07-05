fc2d69f714df0516db5382c584b58513dfa8e745
Sarkis <s.agadzhanyan@poisk-it.ru>
2017-07-05 21:39:02 +0300

    load project

---
+++ App/CollectionHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public static class CollectionHelper
+    {
+        public static void GetPairs(IList<int> collection, int sum)
+        {
+            var dict = new Dictionary<int, int>();
+            var hashSet = new HashSet<string>();
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (!dict.ContainsKey(collection[i]))
+                    dict.Add(collection[i], i);
+            }
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if (dict.ContainsKey(sum - collection[i]) &&
+                    dict[sum - collection[i]] != i &&
+                    collection[i] <= (sum - collection[i]) &&
+                    !hashSet.Contains("" + dict[sum - collection[i]] + collection[i]))
+                {
+                    hashSet.Add("" + dict[sum - collection[i]] + collection[i]);
+                    Console.WriteLine(collection[i].ToString() + " " + (sum - collection[i]).ToString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Sum = 10;
+            var Collection = new List<int> { 1, -2, 0, 2, 1, 4, 5, 5, 5, 5, 2, 3, 4, 20, 5, 6, 7, 8, 9, 5, 0, 10, 6, 7, 8, 9, 19, 29, -10, -19, 3, 33, -23, -90, 100 };
+            CollectionHelper.GetPairs(Collection, Sum);
+        }
+    }
+}
